09f2fbd5e7a084a3e0b583b839e0330f29d9c5bd
VesalTHONG <2021093thong@aupp.edu.kh>
2025-02-07 05:29:15 +0700

    add findTodo() function and fix action handling

--- assignment1/assignment1.js
+++ assignment1/assignment1.js
@@ -44,41 +44,78 @@ function removeTodo() {
   });
 }
 
-function askForTodo() {
+function findTodo() {
   const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
   });
 
-  rl.question("Do you want to add a task? (y/n): ", function (answer) {
-    if (answer.toLowerCase() === "y") {
-      rl.question("Enter the task: ", function (task) {
+  rl.question("Enter the task to find: ", function (task) {
+    let found = false;
+    for (let i = 0; i < todoList.length; i++) {
+      if (todoList[i] === task) {
+        console.log("Found task at position: " + (i + 1));
+        found = true;
+        break;
+      }
+    }
+    if (!found) {
+      console.log("Task not found.");
+    }
+    rl.close();
+    askForTodo();
+  });
+}
+
+function handleAction(action) {
+  switch (action.toLowerCase()) {
+    case "add":
+      const rlAdd = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+      });
+      rlAdd.question("Enter the task: ", function (task) {
         try {
           if (!task) throw new Error("Task cannot be empty");
           addTodo(task);
         } catch (error) {
           console.error(error.message);
         } finally {
-          rl.close();
+          rlAdd.close();
           listTodos();
           askForTodo();
         }
       });
-    } else {
-      rl.question(
-        "Do you want to remove a task? (y/n): ",
-        function (removeAnswer) {
-          if (removeAnswer.toLowerCase() === "y") {
-            rl.close();
-            removeTodo();
-          } else {
-            rl.close();
-            listTodos();
-          }
-        }
-      );
-    }
+      break;
+    case "remove":
+      removeTodo();
+      break;
+    case "find":
+      findTodo();
+      break;
+    case "exit":
+      console.log("Exiting the to-do list application.");
+      break;
+    default:
+      console.log("Invalid option.");
+      listTodos();
+      askForTodo();
+  }
+}
+
+function askForTodo() {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
   });
+
+  rl.question(
+    "What would you like to do? (add/remove/find/exit): ",
+    function (action) {
+      rl.close();
+      handleAction(action);
+    }
+  );
 }
 
 function main() {
